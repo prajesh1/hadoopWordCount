00339680f2a26ef735741a6d85adb6e5dc65339e
prajesh1 <prajesh@tamu.edu>
2016-03-17 21:33:40 -0700

    @Override for reducer and changed its signature

--- WordCounter/src/WordCount.java
+++ WordCounter/src/WordCount.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 import java.util.*;
-        
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
+
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -26,11 +27,13 @@ public class WordCount {
     }
  } 
  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-	    public void reduce(Text key, Iterator<IntWritable> values, Context context) 
+	    @Override			
+	    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
 	      throws IOException, InterruptedException {
+	    	
 	        int sum = 0;
-	        while (values.hasNext()) {
-	            sum += values.next().get();
+	        for (IntWritable val : values) {
+	            sum += val.get();
 	        }
 	        context.write(key, new IntWritable(sum));
 	    }
