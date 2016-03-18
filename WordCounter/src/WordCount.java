a668a6666b9c2546b1fa6c1cd1b690c12acd6a25
prajesh1 <prajesh@tamu.edu>
2016-03-17 21:08:50 -0700

    added    job.setJarByClass(WordCount.class) to fix the error

--- WordCounter/src/WordCount.java
+++ WordCounter/src/WordCount.java
@@ -40,7 +40,7 @@ public class WordCount {
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
         
-        Job job = new Job(conf, "wordcount");
+        Job job = new Job(conf, "wordcount");     
     
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
@@ -54,7 +54,9 @@ public class WordCount {
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
         
+    job.setJarByClass(WordCount.class);
     job.waitForCompletion(true);
+
  }
         
 }
